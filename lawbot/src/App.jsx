6e87528ec5249ef7dc39aae42093c84bb692a388
troy <troypsn17@gmail.com>
2025-11-05 21:36:36 +0800

    Added Landing Page - Hero Section

--- lawbot/src/App.jsx
+++ lawbot/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar.jsx'
+import Landing from './Components/Landing.jsx';
 
 function App() {
-
-
   return (
    <>
     <Navbar/> 
+    <Landing/>
    </>
   );
 }
--- lawbot/src/Components/Landing.jsx
+++ lawbot/src/Components/Landing.jsx
@@ -1,7 +1,23 @@
+import classes from "../Styles/landing.module.css"
+import moralLady from '../assests/moralLady.png'
 function Landing() {
-    return ( 
-        <div>
 
+    return ( 
+        <div className={classes.landingPageContainer}>
+            <div className={classes.hero}>
+                <img src={moralLady} alt="Moral Lady"></img>
+                <p className={classes.title}>
+                    Summarize Legal Documents in Seconds.
+                </p>
+                <div className={classes.uploadBar}>
+                    <p>Upload PNG / Text / PDF / Word Document</p>
+                </div>
+                <div className={classes.subtitle}>
+                    <p> Try it now.</p>
+                </div>
+            </div>
+
+    
         </div>
      );
 }
