8d806f263f1ded0e7e914494ac876ffa224d90f9
Minami189 <Soon128HD@gmail.com>
2025-11-11 13:19:37 +0800

    ito pa pala

--- lawbot/src/App.jsx
+++ lawbot/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './Components/Navbar.jsx'
 import Landing from './Components/Landing.jsx';
 import Login from './Components/Login.jsx';
+import Dashboard from './Components/Dashboard.jsx';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route index element={<Landing/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
    </BrowserRouter>
   );
