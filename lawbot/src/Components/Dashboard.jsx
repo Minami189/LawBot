b88a91741ad110f58d4484f3c70749c07e99b5da
Zeelllllyyy <janzelgarcia967@gmail.com>
2025-11-15 16:27:59 +0800

    dashboard

--- lawbot/src/Components/Dashboard.jsx
+++ lawbot/src/Components/Dashboard.jsx
@@ -9,98 +9,139 @@ import Drives from "../assests/dashboard/HardDrives.png";
 export default function Dashboard() {
   return (
     <div className={classes.DashboardContainer}>
-  <div className={classes.Dashboard}>Dashboard</div>
+      <div className={classes.Dashboard}>Dashboard</div>
 
-  <div className={classes.dashboardText}>
-    See your latest activities, analyzed documents, and insights at a glance.
-    Stay organized and in control of your legal workflow.
-  </div>
-
-  {/* NEW FLEX WRAPPER */}
-  <div className={classes.DashboardMainContent}>
-    {/* LEFT SIDE */}
-    <div className={classes.LeftSection}>
-      <div className={classes.QuickActionsContainer}>
-        <div className={classes.QuickActionsTitle}>Quick Actions</div>
-        <div className={classes.QuickActionsButtons}>
-          {/* Upload New Document */}
-          <div className={classes.ActionBox}>
-            <div className={classes.ActionContent}>
-              <img src={fileIcon} alt="file icon" className={classes.ActionIcon} />
-              <span className={classes.ActionText}>Upload New Document</span>
-            </div>
-          </div>
-
-          {/* View Document History */}
-          <div className={classes.ActionBox}>
-            <div className={classes.ActionContent}>
-              <img src={bx_box} alt="box icon" className={classes.ActionIcon} />
-              <span className={classes.ActionText}>View Document History</span>
-            </div>
-          </div>
-        </div>
+      <div className={classes.dashboardText}>
+        See your latest activities, analyzed documents, and insights at a
+        glance. Stay organized and in control of your legal workflow.
       </div>
 
-      <div className={classes.RecentSummariesContainer}>
-        <div className={classes.RecentSummariesTitle}>Recent Summaries</div>
-        <div className={classes.summaryCard}>
-          <div className={classes.summaryHeader}>
-            <div className={classes.summaryTitle}>
-              Employment Contract - Jordan Poole
-            </div>
-            <div className={classes.summaryDescription}>
-              Topics covered: Legal employment agreement with terms and conditions...
-              <div className={classes.filesDetailsContainer}>
-                <div className={classes.summaryDate}>
-                  <img src={Calendar} alt="Calendar" className={classes.icon} />
-                  November 05, 2025
+      {/* NEW FLEX WRAPPER */}
+      <div className={classes.DashboardMainContent}>
+        {/* LEFT SIDE */}
+        <div className={classes.LeftSection}>
+          <div className={classes.QuickActionsContainer}>
+            <div className={classes.QuickActionsTitle}>Quick Actions</div>
+            <div className={classes.QuickActionsButtons}>
+              {/* Upload New Document */}
+              <div className={classes.ActionBox}>
+                <div className={classes.ActionContent}>
+                  <img
+                    src={fileIcon}
+                    alt="file icon"
+                    className={classes.ActionIcon}
+                  />
+                  <span className={classes.ActionText}>
+                    Upload New Document
+                  </span>
                 </div>
-                <div className={classes.fileType}>
-                  <img src={Vector} alt="Calendar" className={classes.icon} />
-                  PDF
+              </div>
+
+              {/* View Document History */}
+              <div className={classes.ActionBox}>
+                <div className={classes.ActionContent}>
+                  <img
+                    src={bx_box}
+                    alt="box icon"
+                    className={classes.ActionIcon}
+                  />
+                  <span className={classes.ActionText}>
+                    View Document History
+                  </span>
                 </div>
               </div>
             </div>
           </div>
-          <div className={classes.statusBadge}>
-            <span className={classes.statusIcon}>✓</span>
-            Success
-          </div>
-        </div>
-      </div>
-    </div>
 
-    {/* RIGHT SIDE */}
-    <div className={classes.RightSection}>
-      <div className={classes.UsageStatsContainer}>
-        <div className={classes.UsageStatsHeader}>
-          <h3 className={classes.UsageStatsTitle}>Usage Statistics</h3>
-        </div>
+          <div className={classes.RecentSummariesContainer}>
+            <div className={classes.RecentSummariesTitle}>Recent Summaries</div>
 
-        <div className={classes.UsageStatsItem}>
-          <div className={classes.UsageStatsText}>
-            <span className={classes.UsageStatsLabel}>Documents Processed</span>
-            <span className={classes.UsageStatsNumber}>24</span>
-            <span className={classes.UsageStatsInfo}>+14% from last period</span>
-          </div>
-          <img src={Folder} alt="Folder" className={classes.UsageIconImage} />
-        </div>
+            <div className={classes.summaryCard}>
+              {/* LEFT SIDE  Summaries*/}
+              <div className={classes.summaryLeft}>
+                <div className={classes.summaryTitle}>
+                  Employment Contract - Jordan Poole
+                </div>
 
-        <div className={classes.UsageStatsItem}>
-          <div className={classes.UsageStatsText}>
-            <span className={classes.UsageStatsLabel}>Storage Used</span>
-            <span className={classes.UsageStatsNumber}>1.1 gb</span>
-            <div className={classes.UsageBar}>
-              <div className={classes.UsageBarFill}></div>
+                <div className={classes.summaryDescription}>
+                  Topics covered: Legal employment agreement with terms and
+                  conditions...
+                </div>
+
+                <div className={classes.filesDetailsContainer}>
+                  <div className={classes.summaryDate}>
+                    <img
+                      src={Calendar}
+                      alt="Calendar"
+                      className={classes.icon}
+                    />
+                    November 05, 2025
+                  </div>
+
+                  <div className={classes.fileType}>
+                    <img
+                      src={Vector}
+                      alt="File Type"
+                      className={classes.icon}
+                    />
+                    PDF
+                  </div>
+                </div>
+              </div>
+
+              {/* RIGHT SIDE Summaries*/}
+              <div className={classes.statusBadge}>
+                <span className={classes.statusIcon}>✓</span>
+                Success
+              </div>
             </div>
-            <span className={classes.UsageStatsInfo}>47% of 2.5 gb limit</span>
           </div>
-          <img src={Drives} alt="Drives" className={classes.UsageIconImage} />
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className={classes.RightSection}>
+          <div className={classes.UsageStatsContainer}>
+            <div className={classes.UsageStatsHeader}>
+              <h3 className={classes.UsageStatsTitle}>Usage Statistics</h3>
+            </div>
+
+            <div className={classes.UsageStatsItem}>
+              <div className={classes.UsageStatsText}>
+                <span className={classes.UsageStatsLabel}>
+                  Documents Processed
+                </span>
+                <span className={classes.UsageStatsNumber}>24</span>
+                <span className={classes.UsageStatsInfo}>
+                  +14% from last period
+                </span>
+              </div>
+              <img
+                src={Folder}
+                alt="Folder"
+                className={classes.UsageIconImage}
+              />
+            </div>
+
+            <div className={classes.UsageStatsItem}>
+              <div className={classes.UsageStatsText}>
+                <span className={classes.UsageStatsLabel}>Storage Used</span>
+                <span className={classes.UsageStatsNumber}>1.1 gb</span>
+                <div className={classes.UsageBar}>
+                  <div className={classes.UsageBarFill}></div>
+                </div>
+                <span className={classes.UsageStatsInfo}>
+                  47% of 2.5 gb limit
+                </span>
+              </div>
+              <img
+                src={Drives}
+                alt="Drives"
+                className={classes.UsageIconImage}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
   );
 }
