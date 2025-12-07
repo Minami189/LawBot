6f29c322c5ec591015458625c53d5451c6a7043d
Abiel De Luna <deluna.abielpatrick.c@gmail.com>
2025-12-07 23:23:27 +0800

    Sidebar Creation

---
+++ lawbot/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import { ListIcon } from "@phosphor-icons/react";
+import classes from "./Navbar.module.css";
+import logo from "../../assests/logo.png";
+
+
+function Navbar() {
+
+	return (
+		<div className={classes.mainSidebar}>
+			<sidebar className={classes.header}>
+
+			</sidebar>
+		</div>
+  	);
+}
+
+export default Navbar;
